b75b45aefc30926c385a8a892b6bfd603d8a9388
Yash <sahuyash456@gmail.com>
2021-06-11 15:11:14 +0530

    # WARNING: head commit changed in the meantime
    Updated

--- Spring-boot-StockProject/src/main/java/com/stockproject/mysql/entity/ipo.java
+++ Spring-boot-StockProject/src/main/java/com/stockproject/mysql/entity/ipo.java
@@ -8,20 +8,20 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="IPOdb")
+@Table(name="ipodb")
 public class ipo {
-    
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="id")
 	private long id;
-    @Column(name="Company_Name")
-    private String companyName;	
-    @Column(name="Stock_Exchange")
-    private int stockExchange;
-    @Column(name="Price_Per_Share")
-    private double pricePerShare;
-    public ipo(long id, String companyName, int stockExchange, double pricePerShare, int totalShares, String datetime,
+	@Column(name="Company_Name")
+	private String companyName;
+	@Column(name="Stock_Exchange")
+	private int stockExchange;
+	@Column(name="Price_Per_Share")
+	private double pricePerShare;
+
+	public ipo(long id, String companyName, int stockExchange, double pricePerShare, int totalShares, String datetime,
 			String remarks) {
 		super();
 		this.id = id;
@@ -32,57 +32,68 @@ public class ipo {
 		this.datetime = datetime;
 		this.remarks = remarks;
 	}
-	@Column(name=" Total number of Shares")
-    private int totalShares;
-    @Column(name="Open Date Time")
-    private String datetime;
-    @Column(name="Remarks")
-    private String remarks;
+
+	@Column(name="Total_number_of_Shares")
+	private int totalShares;
+	@Column(name="Open_Date_Time")
+	private String datetime;
+	@Column(name="Remarks")
+	private String remarks;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
+
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+
 	public int getStockExchange() {
 		return stockExchange;
 	}
+
 	public void setStockExchange(int stockExchange) {
 		this.stockExchange = stockExchange;
 	}
+
 	public double getPricePerShare() {
 		return pricePerShare;
 	}
+
 	public void setPricePerShare(double pricePerShare) {
 		this.pricePerShare = pricePerShare;
 	}
+
 	public int getTotalShares() {
 		return totalShares;
 	}
+
 	public void setTotalShares(int totalShares) {
 		this.totalShares = totalShares;
 	}
+
 	public String getDatetime() {
 		return datetime;
 	}
+
 	public void setDatetime(String datetime) {
 		this.datetime = datetime;
 	}
+
 	public String getRemarks() {
 		return remarks;
 	}
+
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
 	}
-	
 
-    
-    	
-	
 }
